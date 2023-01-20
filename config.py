f3036433c411d42f58c53ca6b6449bc51cd342db
doc-demen <122993009+doc-demen@users.noreply.github.com>
2023-01-20 05:36:43 -0500

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ if os.path.exists("local.env"):
 load_dotenv()
 class Config(object):
     log = True
-    APP_ID = getenv("API_ID", "6435225")
-    API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")
+    APP_ID = getenv("API_ID", "20414162")
+    API_HASH = getenv("API_HASH", "d76fe451e4a8d163a0468b5da1fb4233")
     TOKEN = getenv("TOKEN", "")
     OWNER_ID = getenv("OWNER_ID", "")
     ASSISTANT_ID = getenv("ASSISTANT_ID", "")
@@ -17,10 +17,10 @@ class Config(object):
     OWNER_USERNAME = getenv("OWNER_USERNAME", "")
     DB_URI = getenv("DATABASE_URL", "")
     DB_URI = DB_URI.replace("postgres", "postgresql")
-    MESSAGE_DUMP = getenv("MESSAGE_DUMP", "-1001509525202")
-    GBAN_LOGS = getenv("GBAN_LOGS", "-1001509525202")
-    SYS_ADMIN = getenv("SYS_ADMIN", "1669178360")
-    DEV_USERS = getenv("DEV_USERS", "1669178360")
+    MESSAGE_DUMP = getenv("MESSAGE_DUMP", "-1001749823528")
+    GBAN_LOGS = getenv("GBAN_LOGS", "-1001749823528")
+    SYS_ADMIN = getenv("SYS_ADMIN", "1128350486")
+    DEV_USERS = getenv("DEV_USERS", "1128350486")
     LOAD = getenv("LOAD")
     WEBHOOK = False
     SPB_MODE = True
@@ -35,8 +35,8 @@ class Config(object):
     BAN_STICKER = getenv("BAN_STICKER", "")
     ALLOW_EXCL = True
     CUSTOM_CMD = False
-    CHANNEL = getenv("CHANNEL", "TheUpdatesChannel")
-    SUPPORT = getenv("SUPPORT", "TheSupportChat")
+    CHANNEL = getenv("CHANNEL", "KK_KanimangalamKovilakam")
+    SUPPORT = getenv("SUPPORT", "KK_KanimangalamKovilakam")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
     CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
     CASH_API_KEY = getenv("CASH_API_KEY", "https://www.alphavantage.co/support/#api-key")
@@ -48,6 +48,6 @@ class Config(object):
     CF_API_KEY = getenv("CF_API_KEY", "coffehouse.intellivoid.net")
     BOT_API_URL = getenv("BOT_API_URL", "https://api.telegram.org/bot")
     BOT_API_FILE_URL = getenv("BOT_API_FILE_URL", "https://api.telegram.org/file/bot")
-    SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1669178360").split()))
-    ZAID_USER = list(map(int, getenv("DEV_USERS", "1669178360").split()))
+    SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1128350486").split()))
+    ZAID_USER = list(map(int, getenv("DEV_USERS", "1128350486").split()))
     NO_LOAD = list(map(int, getenv("NO_LOAD", "").split()))
